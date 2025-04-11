cf86735e1537012cde7e24fe76859062f73bd96e
Debi B <debbieboateng@hotmail.com>
2025-04-11 15:26:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Password-Generator
+ A password generator built with HTML, CSS and JavaScript
---
+++ index.js
@@ -0,0 +1,20 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+let pass1 = document.getElementById("1stGeneratedPassword")
+let pass2 = document.getElementById("2ndGeneratedPassword")
+let generateBtn = document.getElementById("generateBtn")
+
+
+generateBtn.addEventListener("click", function() {  
+    let passField1 = ""
+    let passField2 = ""
+    let length = 15;
+    for (let i = 0; i < length; i++) {
+        let password1 = Math.floor(Math.random()*characters.length)
+        let password2 = Math.floor(Math.random()*characters.length)
+        pass1.textContent = passField1+= characters[password1]
+        pass2.textContent = passField2+= characters[password2]
+        
+    }
+    
+})
